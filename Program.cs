8a9c7fd874b0ca65f35eb1c416f2142b53da9ed4
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-09-12 12:38:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 3;
+
+            Console.WriteLine(a + " dijeljeno sa " + b + " je " + a / b + " i ostatak " + a % b + ".");
+            Console.ReadKey();
+        }
+    }
+}
